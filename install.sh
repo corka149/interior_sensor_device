31e7ff89516c051d641a7ec7a31f1310bad4906c
corka149 <corka149@mailbox.org>
2019-09-24 08:59:41 +0200

    Improved installation

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 sudo mkdir /etc/interior_sensor_device/
 sudo chown $USER:$USER /etc/interior_sensor_device/
 # You must be inside the root folder of this project!
-cp -r "$(pwd)/*" /etc/interior_sensor_device/
+cp -r $(pwd)/* /etc/interior_sensor_device/
 
 # Required
 pip3 install -r /etc/interior_sensor_device/requirements.txt
@@ -15,6 +15,15 @@ sudo chown -R interior_sensor_device_user:interior_sensor_device_user /etc/inter
 sudo chmod +x /etc/interior_sensor_device/start.py
 sudo cp /etc/interior_sensor_device/systemd/interior_sensor_device.service /etc/systemd/system/interior_sensor_device.service
 
+# Hints
+
+printf "
+#######################################################
+# - Edit the start.py in /etc/interior_sensor_device  #
+# - You may what to edit the user of the service.     #
+#######################################################
+
 # Let's start the service
 sudo systemctl enable interior_sensor_device
 sudo systemctl start interior_sensor_device
+"
--- start.py
+++ start.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 from interior_sensor_device import cli, config
 
